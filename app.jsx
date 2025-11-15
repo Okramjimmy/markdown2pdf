c1a86967b0cfc86599cc8755e862c7221446e2fe
Okram Jimmy <flydonkeygod@gmail.com>
2025-11-15 20:03:49 +0530

    Fix import statement syntax in app.jsx
    Fixed import statement syntax in app.jsx.

--- app.jsx
+++ app.jsx
@@ -1,4 +1,4 @@
-import React, 'useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react'; // <-- THIS LINE IS FIXED
 import { Icon } from './Icon'; // Assuming Icon component is in a separate file
 
 
